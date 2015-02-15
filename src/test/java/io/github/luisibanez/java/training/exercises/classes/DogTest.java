91ba3e06da80314e4ffb132efd0cd6ef59b55220
Luis Ibanez <luis.ibanez@gmail.com>
2015-02-15 19:30:36 +0000

    Refined test for Setters and Getters.

--- src/test/java/io/github/luisibanez/java/training/exercises/classes/DogTest.java
+++ src/test/java/io/github/luisibanez/java/training/exercises/classes/DogTest.java
@@ -31,9 +31,42 @@ public class DogTest
     /**
      * Rigourous Test :-)
      */
-    public void testDog()
+    public void testDogSetGetMethods()
     {
-        System.out.println("Whoof!");
-        assertTrue( true );
+        Dog spot = new Dog();
+
+        System.out.print("Testing weight methods...");
+        spot.setWeight(15);
+        assertTrue( spot.weight() == 15 );
+        spot.setWeight(25);
+        assertTrue( spot.weight() == 25 );
+        System.out.println("PASSED!");
+
+        System.out.print("Testing age methods...");
+        spot.setAge(3);
+        assertTrue( spot.age() == 3 );
+        spot.setAge(5);
+        assertTrue( spot.age() == 5 );
+        System.out.println("PASSED!");
+
+        System.out.print("Testing hair color methods...");
+        spot.setHairColor(Dog.HairColor.BLACK);
+        assertTrue( spot.hairColor() == Dog.HairColor.BLACK );
+        spot.setHairColor(Dog.HairColor.WHITE);
+        assertTrue( spot.hairColor() == Dog.HairColor.WHITE );
+        spot.setHairColor(Dog.HairColor.YELLOW);
+        assertTrue( spot.hairColor() == Dog.HairColor.YELLOW );
+        spot.setHairColor(Dog.HairColor.BROWN);
+        assertTrue( spot.hairColor() == Dog.HairColor.BROWN );
+        System.out.println("PASSED!");
+
+        System.out.print("Testing hair length methods...");
+        spot.setHairLength(Dog.HairLength.SHORT);
+        assertTrue( spot.hairLength() == Dog.HairLength.SHORT );
+        spot.setHairLength(Dog.HairLength.LONG);
+        assertTrue( spot.hairLength() == Dog.HairLength.LONG );
+        spot.setHairLength(Dog.HairLength.VERYLONG);
+        assertTrue( spot.hairLength() == Dog.HairLength.VERYLONG );
+        System.out.println("PASSED!");
     }
 }
