b8d0fb745ee15fd2d5fdb6e8e6b1b0ecf562167f
Luis Ibanez <luis.ibanez@gmail.com>
2015-02-15 18:35:59 +0000

    Added Setter and Getter methods.

--- src/main/java/io/github/luisibanez/java/training/exercises/classes/Dog.java
+++ src/main/java/io/github/luisibanez/java/training/exercises/classes/Dog.java
@@ -9,16 +9,48 @@ public class Dog {
   int age;         // measured in integer years
   double speed;    // measured in meters per second
 
-  public enum Color {
+  public enum HairColor {
     BLACK, WHITE, YELLOW, BROWN
   }
 
-  public enum Hair {
+  public enum HairLength {
     SHORT, LONG, VERYLONG
   }
 
-  private Color color;
-  private Hair hair;
+  private HairColor hairColor;
+  private HairLength hairLength;
+
+  public HairColor hairColor() {
+    return this.hairColor;
+  }
+
+  public HairLength hairLength() {
+    return this.hairLength;
+  }
+
+  public double weight() {
+    return this.weight;
+  }
+
+  public int age() {
+    return this.age;
+  }
+
+  public void setHairColor(HairColor color) {
+    this.hairColor = color;
+  }
+
+  public void setHairLength(HairLength length) {
+    this.hairLength = length;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public void setWeight(double weight) {
+    this.weight = weight;
+  }
 
   public void walk() {
     this.speed = 1.4;   // set walking speed
